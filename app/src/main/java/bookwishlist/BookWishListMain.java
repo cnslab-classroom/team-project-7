1f6e0355d1f3f93c54b7ab8b58d32502c27e2500
Seunghwa Ryu <rshh0714@gmail.com>
2024-11-24 20:58:22 +0900

    메뉴 별로 함수로 작성

--- app/src/main/java/bookwishlist/BookWishListMain.java
+++ app/src/main/java/bookwishlist/BookWishListMain.java
@@ -14,38 +14,23 @@ public class BookWishListMain {
 
             switch (input) {
                 case 1: {
-                    service.addCategory(Input.getString("추가할 카테고리 이름을 입력하세요: "));
+                    RunMenu1(service);
                     break;
                 }
                 case 2: {
-                    service.printCategories();
+                    RunMenu2(service);
                     break;
                 }
                 case 3: {
-                    String bookName = Input.getString("추가할 책 이름을 입력하세요: ");
-                    String author = Input.getString("\""+bookName + "\" 의 저자를 입력하세요: ");
-                    String categoryName = Input.getString("\""+bookName + "\" 카테고리를 입력하세요: ");
-                    
-                    // 카테고리가 비어있을 경우 default로 설정
-                    if(categoryName.equals("")) {
-                        categoryName = "default";
-                    }
-
-                    if(service.addBook(bookName, author, categoryName)) {
-                        System.out.printf("\"%s\"이(가) 카테고리 [%s]에 추가되었습니다.\n",bookName, categoryName);
-                    } else {
-                        System.out.println("책 추가 실패!");
-                    }
+                    RunMenu3(service);
                     break;
                 }
                 case 4: {
-                    String name = Input.getString("삭제할 책 이름을 입력하세요: ");
-                    service.deleteBook(name);
+                    RunMenu4(service);
                     break;
                 }
                 case 5: {
-                    String name = Input.getString("책 목록을 볼 카테고리 이름을 입력하세요: ");
-                    service.printBooks(name);
+                    RunMenu5(service);
                     break;
                 }
                 case 6: {
@@ -74,5 +59,45 @@ public class BookWishListMain {
         System.out.println("6. 종료");
         System.out.print("선택: ");
     }
-    
+
+    private static void RunMenu1(BookService service) {
+        service.addCategory(Input.getString("추가할 카테고리 이름을 입력하세요: "));
+    }
+
+    private static void RunMenu2(BookService service) {
+        service.printCategories();
+    }
+
+    private static void RunMenu3(BookService service) {
+        String bookName = Input.getString("추가할 책 이름을 입력하세요: ");
+        String author = Input.getString("\""+bookName + "\" 의 저자를 입력하세요: ");
+        String categoryName = Input.getString("\""+bookName + "\" 카테고리를 입력하세요: ");
+        
+        // 카테고리가 비어있을 경우 default로 설정
+        if(categoryName.equals("")) {
+            categoryName = "default";
+        }
+
+        if(service.addBook(bookName, author, categoryName)) {
+            System.out.printf("\"%s\"이(가) 카테고리 [%s]에 추가되었습니다.\n",bookName, categoryName);
+        } else {
+            System.out.println("책 추가 실패!");
+        }
+    }
+
+    private static void RunMenu4(BookService service) {
+        String name = Input.getString("삭제할 책 이름을 입력하세요: ");
+        service.deleteBook(name);
+    }
+
+    private static void RunMenu5(BookService service) {
+        String categoryName = Input.getString("책 목록을 볼 카테고리 이름을 입력하세요: ");
+        
+        // 카테고리가 비어있을 경우 default로 설정
+        if(categoryName.equals("")) {
+            categoryName = "default";
+        }
+
+        service.printBooks(categoryName);
+    }
 }
