185466be0ea1b06719ee12e100f318a96ed0d4ce
Seunghwa Ryu <rshh0714@gmail.com>
2024-11-29 20:31:52 +0900

    처음 처장시, 초기화 하는 부분 따로 함수로 작성

--- app/src/main/java/bookwishlist/DiskBookRepository.java
+++ app/src/main/java/bookwishlist/DiskBookRepository.java
@@ -24,7 +24,6 @@ public class DiskBookRepository implements BookRepository{
         loadDataFromFile();
     }
 
-
     // 데이터를 JSON 파일에 저장하는 메서드
     public void saveDataToFile() {
         Gson gson = new Gson();
@@ -42,24 +41,23 @@ public class DiskBookRepository implements BookRepository{
     }
 
     // JSON 파일에서 데이터를 불러오는 메서드
-    @SuppressWarnings("unchecked")
     private void loadDataFromFile() {
         Gson gson = new Gson();
         try (FileReader reader = new FileReader(FILE_PATH)) {
+            // JSON 파일에서 전체 데이터를 읽어와 Map<String, Object> 타입으로 변환
             Type dataType = new TypeToken<Map<String, Object>>(){}.getType();
             Map<String, Object> data = gson.fromJson(reader, dataType);
 
+            // booksStore 데이터를 JSON 문자열로 변환한 후, Map<Long, Book> 타입으로 역직렬화
             booksStore = gson.fromJson(gson.toJson(data.get("booksStore")), new TypeToken<Map<Long, Book>>(){}.getType());
+            // categoriesStore 데이터를 JSON 문자열로 변환한 후, Map<String, Category> 타입으로 역직렬화
             categoriesStore = gson.fromJson(gson.toJson(data.get("categoriesStore")), new TypeToken<Map<String, Category>>(){}.getType());
 
             // data.get("id")가 Double, Integer, 또는 다른 숫자 타입일 수 있기에 Number로 캐스팅 후 변환
             id = ((Number) data.get("id")).longValue();
         } 
         catch (FileNotFoundException e) { // 파일이 존재하지 않을 경우 초기화
-            booksStore = new HashMap<>();
-            categoriesStore = new HashMap<>();
-            categoriesStore.put("default", new Category("default"));
-            id = 0L;
+            initializeDefaults();
         } 
         catch (IOException e) {
             e.printStackTrace();
@@ -129,15 +127,11 @@ public class DiskBookRepository implements BookRepository{
         return result;
     }
 
-    private void printBooks(Map<Long, Book> map) {
-        for (Map.Entry<Long, Book> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
-    }
-
-    private void printCategories(Map<String, Category> map) {
-        for (Map.Entry<String, Category> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
+    // 기본 상태로 초기화
+    private void initializeDefaults() {
+        booksStore = new HashMap<>();
+        categoriesStore = new HashMap<>();
+        categoriesStore.put("default", new Category("default"));
+        id = 0L;
     }
 }
